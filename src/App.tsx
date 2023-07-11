6821d35f879771efb932abac843eda5eba29cb6e
Adrian Hernandez <danix08@live.com>
2023-07-11 18:16:35 -0300

    Preparado el estado y dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,73 @@
+import { useReducer } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { type State, type Action } from './types.d'
 
 
+//1. Create a initialSate
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result:'',
+  loading: false,
+  
+}
 
+//2. Create Reducer
+
+function reducer(state: State,action: Action){
+
+    const { type  } = action
+
+    if(type === 'INTERCHANGE_LANGUAGE'){
+      return{
+        ...state,
+        fromLanguage: state.toLanguage,
+        toLanguage: state.fromLanguage
+      }
+    }
+
+    if(type === 'SET_FROM_LANGUAGE'){
+      return{
+        ...state,
+        fromLanguage: action.payload
+      }
+    }
+
+    if(type === 'SET_TO_LANGUAGE'){
+      return{
+        ...state,
+        toLanguage: action.payload
+      }
+    }
+
+    if(type === 'SET_FROM_TEXT'){
+      return{
+        ...state,
+        loading: true,
+        fromText: action.payload,
+        result: ''
+
+      }
+    }
+
+    if(type === 'SET_RESULT'){
+      return{
+        ...state,
+        result: action.payload
+      }
+    }
+
+
+    return state
+
+}
 function App () {
+
+  //3. Usando el hook useReducer
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className='App'>
       <h1>Google Translate </h1>
