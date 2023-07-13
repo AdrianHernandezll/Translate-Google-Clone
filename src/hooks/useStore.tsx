52bb38d88577bb1bc3753bcee33d02852a5914f8
Adrian Hernandez <danix08@live.com>
2023-07-13 16:49:28 -0300

    Creando custom hook y optimizando codigo

---
+++ src/hooks/useStore.tsx
@@ -0,0 +1,86 @@
+import { type State, type Action } from '../types.d'
+
+
+//1. Create a initialSate
+
+
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result:'',
+  loading: false,
+  
+}
+
+//2. Create Reducer
+
+function reducer(state: State,action: Action){
+
+    const { type  } = action
+
+    if(type === 'INTERCHANGE_LANGUAGE'){
+      return{
+        ...state,
+        fromLanguage: state.toLanguage,
+        toLanguage: state.fromLanguage
+      }
+    }
+
+    if(type === 'SET_FROM_LANGUAGE'){
+      return{
+        ...state,
+        fromLanguage: action.payload
+      }
+    }
+
+    if(type === 'SET_TO_LANGUAGE'){
+      return{
+        ...state,
+        toLanguage: action.payload
+      }
+    }
+
+    if(type === 'SET_FROM_TEXT'){
+      return{
+        ...state,
+        loading: true,
+        fromText: action.payload,
+        result: ''
+
+      }
+    }
+
+    if(type === 'SET_RESULT'){
+      return{
+        ...state,
+        result: action.payload
+      }
+    }
+
+
+    return state
+
+}
+
+export function useStore(){
+     //3. Usando el hook useReducer
+  const [
+    {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading
+    }, dispatch] = useReducer(reducer, initialState)
+
+    return {
+     fromLanguage,
+     toLanguage,
+     fromText,
+     result,
+     loading
+    }
+
+    
+}
