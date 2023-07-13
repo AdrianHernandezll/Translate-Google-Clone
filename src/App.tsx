78ea58ff832cc01d0dd0ec5ed3ed47c399d38efa
Adrian Hernandez <danix08@live.com>
2023-07-13 17:30:01 -0300

    Agregando ReactBootstrap a la interfaz

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+
+
+import './App.css'
 import { useStore } from './hooks/useStore'
 
 
@@ -7,17 +13,28 @@ import { useStore } from './hooks/useStore'
 
 function App () {
 
-  const { fromLanguage, setFromLanguaje} = useStore();
+  const { fromLanguage,toLanguage, setFromLanguaje,interchangeLanguages} = useStore();
   return (
-    <div className='App'>
-      <h1>Google Translate </h1>
-      <button onClick={() =>{
-        setFromLanguaje('es')
-      }}>
-        Cambiar a Español
-      </button>
-      {fromLanguage}
-    </div>
+    <Container fluid>
+        <h1>Google Translate </h1>
+        <Row>
+          <Col>
+            <h2>From</h2>
+            {fromLanguage}
+          </Col>
+          <Col>
+            <button onClick={interchangeLanguages}>
+                Change
+            </button>
+          </Col>
+
+          <Col>
+            <h2>To</h2>
+            {toLanguage}
+          </Col>
+
+        </Row>
+    </Container>
   )
 }
 
